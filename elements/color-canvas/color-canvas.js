458b06b72617fa01504cd67e3962d54d689788c9
Simon <simonxd@gmail.com>
2015-11-05 19:08:49 -0200

    initial

---
+++ elements/color-canvas/color-canvas.js
@@ -0,0 +1,109 @@
+(function () {
+  'use strict';
+
+  Polymer({
+    is: 'color-canvas',
+
+    properties: {
+      value: {
+        type: String,
+        notify: true,
+        value: '',
+        observer: '_handleValueChange'
+      },
+
+      columns: {
+        type: Number,
+        notify: true,
+      },
+
+      charWidth: {
+        type: Number,
+        notify: true,
+      },
+
+      charHeight: {
+        type: Number,
+        notify: true
+      },
+
+      height: {
+        type: Number,
+        notify: true,
+      },
+
+      width: {
+        type: Number,
+        notify: true
+      },
+
+      colorMapping: {
+        type: Object,
+        notify: true,
+        value: {
+          '0': 'black',
+          '1': 'white',
+        }
+      }
+    },
+
+    _handleValueChange: function (value, old) {
+
+      var splitRegExp = new RegExp('.{1,' + this.get('columns') + '}', 'g');
+
+      var lines = value.match(splitRegExp) || [];
+
+      var charWidth  = this.get('charWidth');
+      var charHeight = this.get('charHeight');
+
+      this.clear();
+
+      var colorMapping = this.get('colorMapping');
+
+      // draw for each line
+      lines.forEach(function (lineContents, lineIndex) {
+        Array.prototype.forEach.call(lineContents, function (ch, chIndex) {
+
+          // console.log('print at line %s and char %s', lineIndex, chIndex);
+
+          // new Rectangle(x, y, width, height)
+          var rectangle = new paper.Rectangle(
+            chIndex * charWidth,
+            lineIndex * charHeight,
+            charWidth,
+            charHeight
+          );
+
+          var path = new paper.Path.Rectangle(rectangle);
+
+          path.fillColor = colorMapping[ch] || 'red';
+        })
+      });
+
+
+      // draw at end
+      if (paper.view) {
+        paper.view.draw();
+      }
+    },
+
+    ready: function () {
+      // Get a reference to the canvas object
+      var canvas = this.$.canvas
+      // Create an empty project and a view for the canvas:
+      paper.setup(canvas);
+    },
+
+    clear: function () {
+      // clean up canvas.. this is not the paper way but who cares?
+      if (paper.project && paper.project.activeLayer) {
+        paper.project.activeLayer.removeChildren();
+      }
+
+      var canvas = this.$.canvas;
+
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+    }
+  })
+})();
---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener('WebComponentsReady', function () {
+
+  var machine = window.machine = document.querySelector('#m');
+  var textEditor = document.querySelector('#text-editor');
+  var canvas = document.querySelector('#canvas');
+  var workspace = document.querySelector('#workspace');
+
+  // workspace sizing
+  textEditor.addEventListener('editor-width-changed', setWorkspaceSizing);
+  textEditor.addEventListener('editor-height-changed', setWorkspaceSizing);
+
+  function setWorkspaceSizing() {
+    // should be inside event listener
+    workspace.style.width = textEditor.get('editorWidth') + 'px';
+    workspace.style.height = textEditor.get('editorHeight') + 'px';
+  }
+
+  // clear
+  textEditor.addEventListener('columns-changed', clear);
+  textEditor.addEventListener('rows-changed', clear);
+  textEditor.addEventListener('font-size-changed', clear);
+
+  function clear() {
+    console.log('clear')
+    canvas.clear();
+  }
+
+  // SET VALUES ONTO SCOPE
+  machine.set('columns', 60);
+  machine.set('rows', 15);
+  textEditor.editor.setValue(Array(60 * 15 + 1).join("0"));
+
+  setTimeout(function () {
+    machine.set('showEditor', false);
+  }, 1000)
+
+
+  // immediately invoke
+  setWorkspaceSizing();
+});
